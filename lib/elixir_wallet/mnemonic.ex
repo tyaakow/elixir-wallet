1b4893a8f7939b738456d9f3db1dc9e0e7127614
d-velev <bamzorlol@gmail.com>
2017-09-21 17:34:51 +0300

    The Mnemonic module now generates a compressed master public key

--- lib/elixir_wallet/mnemonic.ex
+++ lib/elixir_wallet/mnemonic.ex
@@ -23,11 +23,19 @@ defmodule Mnemonic do
   end
 
   def generate_master_keys(seed) do
-    seed_binary = seed |> Bits.extract |> Enum.join
     <<private::size(256),chain_code::binary>> = seed
     private_hex = <<private::256>> |> Base.encode16
-    {public,private} = :crypto.generate_key(:ecdh,:secp256k1,private_hex)
-    {private,public,chain_code}
-  end
+    {public, _} = :crypto.generate_key(:ecdh,:secp256k1,private_hex)
+    second_half = public|>Base.encode16 |> String.slice(2,128) |> String.slice(64,64)
+    first_half = public|>Base.encode16 |> String.slice(2,128) |> String.slice(0,64)
+    {last_digit_int,_} = second_half |> String.slice(63,63)|>Integer.parse(16)
+    public_short = cond do
+      rem(last_digit_int,2) == 0 ->
+        "02" <> first_half
+      rem(last_digit_int,2) != 0 ->
+        "03" <> first_half
+    end
+    {private_hex,public_short,chain_code}
+   end
 
 end
