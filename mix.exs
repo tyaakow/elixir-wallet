c63088a78d23c7f71b1e03a505dd55b13fddf9f9
thepiwo <github@piwo.space>
2017-11-14 13:55:46 +0200

    [GH-#56] code coverage tool, for ci to work

--- mix.exs
+++ mix.exs
@@ -7,7 +7,9 @@ defmodule ElixirWallet.Mixfile do
       version: "0.1.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test]
     ]
   end
 
@@ -24,7 +26,8 @@ defmodule ElixirWallet.Mixfile do
       {:base58check, github: "quanterall/base58check"},
       {:seed_generator, github: "quanterall/seed_generator"},
       {:httpoison, "~> 0.13.0"},
-      {:poison, "~> 3.1"}
+      {:poison, "~> 3.1"},
+      {:excoveralls, "~> 0.7", only: :test}
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
