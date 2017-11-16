78ff239946a3fe0fa5f440eea680188e7e5ce350
Boychev-Dimitar <dimitar.boychev@quanterall.com>
2017-11-16 12:19:54 +0200

    [GH-#59] Mnemonic encryption password now submitted by user

--- lib/elixir_wallet/elixir_wallet.ex
+++ lib/elixir_wallet/elixir_wallet.ex
@@ -17,7 +17,7 @@ defmodule Wallet do
       sand trip update spring
   """
   @spec create_wallet(String.t()) :: String.t()
-  def create_wallet(password \\ "") do
+  def create_wallet(password \\ " ") do
 
     mnemonic_phrase = Mnemonic.generate_phrase(GenerateIndexes.generate_indexes)
     save_wallet_file(mnemonic_phrase, password)
@@ -32,7 +32,7 @@ defmodule Wallet do
   If the wallet was not password protected, just pass the mnemonic_phrase
   """
   @spec import_wallet(String.t(), String.t()) :: String.t()
-  def import_wallet(mnemonic_phrase, password \\ "") do
+  def import_wallet(mnemonic_phrase, password \\ " ") do
 
     save_wallet_file(mnemonic_phrase, password)
     Logger.info("You have successfully imported a wallet")
@@ -40,17 +40,23 @@ defmodule Wallet do
 
   @doc """
   Decrypts a file and displays it's mnemonic phrase 
+  Will only return a phrase if the password is correct  
   ## Examples
       iex> Wallet.load_wallet_file("wallet--2017-10-31-14-54-39", "password")
 	  {:ok,
 	  "spirit\r beach\r smile\r turn\r glance\r whale\r rack\r reflect\r marble\r cover\r enter\r pigeon"}
   """
   @spec load_wallet_file(String.t(), String.t()) :: Tuple.t()
-  def load_wallet_file(file_path, password) do
+  def load_wallet_file(file_path, password \\ " ") do
     case File.read(file_path) do
       {:ok, encrypted_data} ->
         mnemonic = Cypher.decrypt(encrypted_data, password)
-	{:ok, mnemonic}
+        if (String.valid? mnemonic) do 
+	        {:ok, mnemonic}
+        else
+          Logger.info("Invalid password")
+          {:error, ""}
+        end  
       {:error, :enoent} ->
         {:error, "The file does not exist."}
       {:error, :eaccess} ->
@@ -70,35 +76,46 @@ defmodule Wallet do
     file = "wallet--#{year}-#{month}-#{day}-#{hours}-#{minutes}-#{seconds}"
     {:ok, file} = File.open(file, [:write])
 
-    ## TODO: Get the password from the user
-    encrypted = Cypher.encrypt(mnemonic_phrase, "password")
+    encrypted = Cypher.encrypt(mnemonic_phrase, password)
     IO.binwrite(file, encrypted)
     File.close(file)
   end
 
   @doc """
   Gets the public key
+  Will only return a public key if the password is correct  
   ## Examples
       iex> Wallet.get_public_key("wallet--2017-10-31-14-54-39", "password")
 	   "04187ACD56365459C0948953DD0D36A93E57A4C78C6E04E1E9DB10EA2C89FF2701DCC1AFB2C30F09ACE265EA11DAFE7E40591182F4EC3E7BB535305551D2A374CB"
   """
   @spec get_public_key(String.t(), String.t()) :: String.t()
-  def get_public_key(file_path, password) do
-   {_, mnemonic} = load_wallet_file(file_path, password)
+  def get_public_key(file_path, password \\ " ") do
+  {validation, mnemonic} = load_wallet_file(file_path, password)
      
-   KeyPair.generate_root_seed(mnemonic)
-   |> elem(1)
+  if (validation != :error) do 
+   public_key = Kernel.elem(KeyPair.generate_root_seed(mnemonic), 1) 
+   {:ok, public_key}
+  else
+   {:error, ""}
   end  
+ end
   
   @doc """
   Gets the wallet address
+  Will only return an address if the password is correct  
   ## Examples
       iex> Wallet.get_address("wallet--2017-10-31-14-54-39", "password")
 	  "1NM51tw1MixFCe64g6ExhCEXnowEGrQ2DE"
   """
   @spec get_address(String.t(), String.t()) :: String.t()
-  def get_address(file_path, password) do
-   get_public_key(file_path, password)
-   |> KeyPair.generate_wallet_address() 
+  def get_address(file_path, password \\ " ") do
+   {validation, public_key} = get_public_key(file_path, password)
+
+  if (validation != :error) do 
+   address = KeyPair.generate_wallet_address(public_key) 
+   {:ok, address}
+  else
+   {:error, ""}
+  end  
   end
 end
