88f4f4be69b514b54b4dc3b394ef570fc6f34914
Boychev-Dimitar <dimitar.boychev@quanterall.com>
2017-11-22 10:39:52 +0200

    [GH-#58] Added requested changes

--- lib/elixir_wallet/elixir_wallet.ex
+++ lib/elixir_wallet/elixir_wallet.ex
@@ -42,15 +42,15 @@ defmodule Wallet do
   Decrypts a file and displays it's mnemonic phrase 
   ## Examples
       iex> Wallet.load_wallet_file("wallet--2017-10-31-14-54-39", "password")
-	  {:ok,
-	  "spirit\r beach\r smile\r turn\r glance\r whale\r rack\r reflect\r marble\r cover\r enter\r pigeon"}
+      {:ok,
+      "spirit\r beach\r smile\r turn\r glance\r whale\r rack\r reflect\r marble\r cover\r enter\r pigeon"}
   """
   @spec load_wallet_file(String.t(), String.t()) :: Tuple.t()
   def load_wallet_file(file_path, password) do
     case File.read(file_path) do
       {:ok, encrypted_data} ->
         mnemonic = Cypher.decrypt(encrypted_data, password)
-	{:ok, mnemonic}
+  {:ok, mnemonic}
       {:error, :enoent} ->
         {:error, "The file does not exist."}
       {:error, :eaccess} ->
@@ -80,25 +80,40 @@ defmodule Wallet do
   Gets the public key
   ## Examples
       iex> Wallet.get_public_key("wallet--2017-10-31-14-54-39", "password")
-	   "04187ACD56365459C0948953DD0D36A93E57A4C78C6E04E1E9DB10EA2C89FF2701DCC1AFB2C30F09ACE265EA11DAFE7E40591182F4EC3E7BB535305551D2A374CB"
+      <<4, 210, 200, 166, 81, 219, 54, 116, 39, 64, 199, 57, 55, 152, 204, 119, 237,
+      168, 175, 243, 132, 39, 71, 208, 94, 138, 190, 242, 78, 74, 141, 43, 58, 241,
+      15, 19, 179, 45, 42, 79, 118, 24, 160, 20, 64, 178, 109, 124, 172, 127, ...>>
   """
-  @spec get_public_key(String.t(), String.t()) :: String.t()
+  @spec get_public_key(String.t(), String.t()) :: Binary.t()
   def get_public_key(file_path, password) do
    {_, mnemonic} = load_wallet_file(file_path, password)
-     
-   KeyPair.generate_root_seed(mnemonic)
-   |> elem(1)
-  end  
+   public_key = Kernel.elem(KeyPair.generate_root_seed(mnemonic), 1)  
+   Base.decode16(public_key)
+   end  
   
   @doc """
   Gets the wallet address
   ## Examples
       iex> Wallet.get_address("wallet--2017-10-31-14-54-39", "password")
-	  "1NM51tw1MixFCe64g6ExhCEXnowEGrQ2DE"
+      "1NM51tw1MixFCe64g6ExhCEXnowEGrQ2DE"
   """
   @spec get_address(String.t(), String.t()) :: String.t()
   def get_address(file_path, password) do
    get_public_key(file_path, password)
    |> KeyPair.generate_wallet_address() 
+  end
+
+  @doc """
+  Gets the private key
+  ## Examples
+      iex> Wallet.get_private_key("wallet--2017-10-31-14-54-39", "password")
+      <<100, 208, 92, 132, 43, 104, 6, 55, 125, 18, 18, 215, 98, 8, 245, 12, 78, 92,
+      89, 115, 59, 231, 28, 142, 137, 119, 62, 19, 102, 238, 171, 185>>
+  """
+  @spec get_private_key(String.t(), String.t()) :: Binary.t()
+  def get_private_key(file_path, password) do
+    {_, mnemonic} = Wallet.load_wallet_file(file_path, password)   
+    private_key = Kernel.elem(KeyPair.generate_root_seed(mnemonic), 0)
+    Base.decode16(private_key)
   end
 end
---
+++ lib/elixir_wallet/signing.ex
@@ -0,0 +1,43 @@
+defmodule Signing do
+
+  @moduledoc """
+  Module for signing and verifying transactions using the ECDSA algorithm
+  """
+
+  @doc """
+   Signs a transaction using a private key
+  ## Examples
+      iex> Signing.sign(<<0,1>>, <<100, 208, 92, 132, 43, 104, 6, 55, 125, 18, 18, 215, 98, 8, 245, 12, 78, 92,
+      89, 115, 59, 231, 28, 142, 137, 119, 62, 19, 102, 238, 171, 185>>)
+      <<48, 70, 2, 33, 0, 228, 179, 41, 106, 184, 31, 168, 184, 227, 240, 145, 166,
+      93, 47, 64, 11, 104, 94, 240, 192, 240, 229, 55, 137, 226, 15, 119, 156, 52,
+      165, 170, 163, 2, 33, 0, 234, 132, 82, 122, 244, 209, 148, 232, 124, 216, ...>>
+  """
+  @spec sign(Binary.t(), Binary.t()) :: Binary.t()
+  def sign(message, privkey_bin) do
+    signature = :crypto.sign(:ecdsa, :sha256, message, [privkey_bin, :secp256k1])
+  end
+
+  @doc """
+   Verifies a signed transaction using a public key 
+  ## Examples
+      iex> Signing.verify
+      (<<0,1>>, 
+      <<48, 70, 2, 33, 0, 228, 179, 41, 106, 184, 31, 168, 184, 227, 240, 145, 166,
+      93, 47, 64, 11, 104, 94, 240, 192, 240, 229, 55, 137, 226, 15, 119, 156, 52,
+      165, 170, 163, 2, 33, 0, 234, 132, 82, 122, 244, 209, 148, 232, 124, 216, ...>>, 
+      <<4, 210, 200, 166, 81, 219, 54, 116, 39, 64, 199, 57, 55, 152, 204, 119, 237,
+      168, 175, 243, 132, 39, 71, 208, 94, 138, 190, 242, 78, 74, 141, 43, 58, 241,
+      15, 19, 179, 45, 42, 79, 118, 24, 160, 20, 64, 178, 109, 124, 172, 127, ...>>)
+      
+      {:ok, true}
+  """
+  @spec verify(Binary.t(), Binary.t(), Binary.t()) :: Tuple.t()
+  def verify(message, signature_bin, pubkey_bin) do
+    if (:crypto.verify(:ecdsa, :sha256, message, signature_bin, [pubkey_bin, :secp256k1])) do
+      {:ok, true}
+    else
+      {:error, false}
+    end 
+  end
+end
