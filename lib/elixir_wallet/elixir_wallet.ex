d3e1872a9415ead7199978178d56aac07a77dd73
artur64 <artur.kratt@quanterall.com>
2017-11-22 17:51:01 +0200

    Fixed tests,changed conversion functionality from hex to bin.

--- lib/elixir_wallet/elixir_wallet.ex
+++ lib/elixir_wallet/elixir_wallet.ex
@@ -50,18 +50,18 @@ defmodule Wallet do
     case File.read(file_path) do
       {:ok, encrypted_data} ->
         mnemonic = Cypher.decrypt(encrypted_data, password)
-      {:ok, mnemonic}
-        {:error, :enoent} ->
-          {:error, "The file does not exist."}
-        {:error, :eaccess} ->
-          {:error, "Missing permision for reading the file,
-          or for searching one of the parent directories."}
-        {:error, :eisdir} ->
-          {:error, "The named file is a directory."}
-        {:error, :enotdir} ->
-          {:error, "A component of the file name is not a directory."}
-        {:error, :enomem} ->
-          {:error, "There is not enough memory for the contents of the file."}
+  {:ok, mnemonic}
+      {:error, :enoent} ->
+        {:error, "The file does not exist."}
+      {:error, :eaccess} ->
+        {:error, "Missing permision for reading the file,
+        or for searching one of the parent directories."}
+      {:error, :eisdir} ->
+        {:error, "The named file is a directory."}
+      {:error, :enotdir} ->
+        {:error, "A component of the file name is not a directory."}
+      {:error, :enomem} ->
+        {:error, "There is not enough memory for the contents of the file."}
     end
   end
 
@@ -88,7 +88,6 @@ defmodule Wallet do
   def get_public_key(file_path, password) do
    {_, mnemonic} = load_wallet_file(file_path, password)
    public_key = Kernel.elem(KeyPair.generate_root_seed(mnemonic), 1)  
-   Base.decode16(public_key)
    end  
   
   @doc """
@@ -114,6 +113,5 @@ defmodule Wallet do
   def get_private_key(file_path, password) do
     {_, mnemonic} = Wallet.load_wallet_file(file_path, password)   
     private_key = Kernel.elem(KeyPair.generate_root_seed(mnemonic), 0)
-    Base.decode16(private_key)
   end
 end
--- test/public_key_and_address_test.exs
+++ test/public_key_and_address_test.exs
@@ -9,7 +9,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "0450863AD64A87AE8A2FE83C1AF1A8403CB53F53E486D8511DAD8A04887E5B23522CD470243453A299FA9E77237716103ABC11A1DF38855ED6F2EE187E9C582BA6" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "16UwLL9Risc3QfPqBUvKofHmBQ7wMtjvM"
   end
 
@@ -20,7 +21,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "040062311DD98C991BB7EA254969273E8780E60926FCD5D8B927583F00F1362880CC85D2BF3106FAE1DBCB1EC2146DB236110C84304EAEFA152F2C89DE98E1AF92"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "040062311DD98C991BB7EA254969273E8780E60926FCD5D8B927583F00F1362880CC85D2BF3106FAE1DBCB1EC2146DB236110C84304EAEFA152F2C89DE98E1AF92" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1AuoyK3Kq8rQ9umw5FpPHQPaDGFTVceBUA"
   end
 
@@ -31,7 +33,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "04E8ABC902A5BAA259AEFAFE16AA3BA5A0ADB1408440A37804D6983997688C81712CAED5A9B99A0D9CF7BC1E005773B298F09720F507660E567CEA8C46C23AFAAC"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "04E8ABC902A5BAA259AEFAFE16AA3BA5A0ADB1408440A37804D6983997688C81712CAED5A9B99A0D9CF7BC1E005773B298F09720F507660E567CEA8C46C23AFAAC" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1DkASREvmL4KVkTrPEJJ7ESes2Nb4PpChm"
   end
 
@@ -42,7 +45,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "040EEA2F51760E5332D2200CD2AFF29922D08A6CC2066A08D2ADF0548AE2014753A44355BFE4DB4556DCE3B4865C297EC22841A98DE5CD596A7E75D7CC290E1F0C"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "040EEA2F51760E5332D2200CD2AFF29922D08A6CC2066A08D2ADF0548AE2014753A44355BFE4DB4556DCE3B4865C297EC22841A98DE5CD596A7E75D7CC290E1F0C" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1L5bKx76C9QeXdFojue5m8KAgtTgGW6EnZ"
   end
 
@@ -53,7 +57,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "043DFF8563882A3E66E84FEB90BFB9FACF80332DD6EB5972482472EDA319828114BDCF807B94C0E389CA285EEF064C0DF4BB188406E50E780427E35BAB893D3217"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "043DFF8563882A3E66E84FEB90BFB9FACF80332DD6EB5972482472EDA319828114BDCF807B94C0E389CA285EEF064C0DF4BB188406E50E780427E35BAB893D3217" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1PBCsF6Fqn1FRHqaNbYKjzjCtrLpcuADot"
   end
 
@@ -64,7 +69,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "04C9D3051C53339040917770A1E04CE6E2A9638DE5BCBCB40FBEC33615A7B3B767235AEF0DE0189A785EAD9ED455F6A6163AF3E15B8C60911F05251172DB18ACC5"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "04C9D3051C53339040917770A1E04CE6E2A9638DE5BCBCB40FBEC33615A7B3B767235AEF0DE0189A785EAD9ED455F6A6163AF3E15B8C60911F05251172DB18ACC5" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1AQRJkftCgJ8eeApUFceFMo9YpUEB9tPhS"
   end
 
@@ -75,7 +81,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "04B0E9E92F95E173B920E4CC32E482B23CC38BF26C99852C7959DDC4698E3334764145ABEB20A365B67AC8B51B67756955E62CD6CCD26B6C6BC3A7CF635E272903"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "04B0E9E92F95E173B920E4CC32E482B23CC38BF26C99852C7959DDC4698E3334764145ABEB20A365B67AC8B51B67756955E62CD6CCD26B6C6BC3A7CF635E272903" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1PPwkXoPreMTs69aScGiesoTudap7M19Es"
   end
 
@@ -86,7 +93,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "0488921A1FF2AA5BB571486AA7588655C6C6D1A68FBB3E0908659ED797034C5EFB5129084FF94F442C8D1FF3BE461FCAA1FD17752F703F3AA045B7F66DFEDF8A7E"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "0488921A1FF2AA5BB571486AA7588655C6C6D1A68FBB3E0908659ED797034C5EFB5129084FF94F442C8D1FF3BE461FCAA1FD17752F703F3AA045B7F66DFEDF8A7E" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "19bmCy3jjuB1X8wks9rTCHkaprD2hPtG3S"
   end
 
@@ -97,7 +105,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "0416FC79A80627363BBDD8A0CC059DAC6529982D0AE78B560AF02828E0C0D9070A6BF03CE95ACB49B9BDFCC8A0D2D0C0C65854E92E8BC5019A604B3486F251D148"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "0416FC79A80627363BBDD8A0CC059DAC6529982D0AE78B560AF02828E0C0D9070A6BF03CE95ACB49B9BDFCC8A0D2D0C0C65854E92E8BC5019A604B3486F251D148" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "13xoFme8mGzVxr3o2J4wpHtEqbfML2CtEd"
   end
 
@@ -108,7 +117,8 @@ defmodule PublicKeyAndAddressTest do
     public_key_bin = KeyPair.generate_master_public_key(pivate_key_dec)
     public_key_hex = public_key_bin |> Base.encode16()
     assert public_key_hex == "041E2302BB6B2AD1222DA0E8D12FF0DD7705EA476E9E6CD589D389A4A2E2BF00A682C0FF225578E2A2DBAA39D00993C40E1E8A352179A359B910380E8EFE0BAF3D"
-    address = KeyPair.generate_wallet_address(public_key_hex)
+    {_, public_key_bin} = "041E2302BB6B2AD1222DA0E8D12FF0DD7705EA476E9E6CD589D389A4A2E2BF00A682C0FF225578E2A2DBAA39D00993C40E1E8A352179A359B910380E8EFE0BAF3D" |> Base.decode16()
+    address = KeyPair.generate_wallet_address(public_key_bin)
     assert address == "1G2T2Ker5AA9V7e7UMdj2umkGhYaXEAmit"
   end
 
